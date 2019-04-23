f34dfc5d6b0959a7eda66f7f0328585ffd0ad716
Thanh Tran <thanh.tran3@metropolia.fi>
2019-04-23 14:53:34 +0300

    extract fileType from filename

--- controllers/upload.js
+++ controllers/upload.js
@@ -3,15 +3,16 @@ const FileModel = require('./database').FileModel;
 
 exports.uploadFile = (req, res) => {
   if(req.file){
-    console.log("something is wrong");
     const fileMover = new FileMover('public', req.body.location);
+    //console.log(req.file.filename);
+
 
     fileMover.save(req.file.filename, (isSuccess, curPath) => {
       if(isSuccess){
         FileModel.create({
           name: req.body.name,
           time: req.body.time,
-          type: req.body.type,
+          type: getFileType(req.file.filename),
           location: req.body.location,
           original: curPath
         }).then((result) => {
@@ -29,3 +30,12 @@ exports.uploadFile = (req, res) => {
     res.status(401).send("Failed upload: File empty");
   }
 };
+
+const getFileType = (filename) => {
+  const splitString = filename.split(".");
+  let fileType = "folder";
+  if(splitString.length !== 0){
+    fileType = splitString[splitString.length - 1]
+  }
+  return fileType
+}
